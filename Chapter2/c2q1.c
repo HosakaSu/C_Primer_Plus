3689edbd3a134034f4ad8625518a8b34afdf8742
cat4coffee <liucjseu@outlook.com>
2020-11-02 23:22:00 +0800

    init c pratise project

---
+++ Chapter2/c2q1.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(void)
+{
+	printf("Sirius Black\r\n");
+	printf("Sirius\r\nBlack\r\n");
+	printf("Sirius");
+	printf("\nBlack\n");
+}
---
+++ Chapter2/c2q2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("Sirius Black\r\n");
+    printf("Province Jiangsu\r\n");
+}
